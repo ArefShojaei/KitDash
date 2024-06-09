99c995de35efa2a58f7dcbcf90ca490e76e954f8
ArefShojaei <arefshojaei82dev@gmail.com>
2024-06-09 13:21:52 +0330

    ADD: add Str util

---
+++ src/Contracts/Interfaces/String/Str.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Kit\Contracts\Interfaces\String;
+
+
+
+/**
+ * Str interface
+ * @interface
+ */
+interface Str {}
---
+++ src/Str.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Kit;
+
+
+/**
+ * @package
+ */
+use Kit\Contracts\Interfaces\String\Str as StrContract;
+
+
+
+/**
+ * String Util
+ * @class
+ * @implements StrContract
+ */
+class Str implements StrContract {
+    /**
+     * Constructor
+     * @private
+     */
+    private function __construct() {}
+}
