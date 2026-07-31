6e8b806a7bfaf6d8afea60c615ae3d47a746254d
ArefShojaei <arefshojaei82@gmail.com>
2026-07-31 15:43:11 +0330

    test: refactor Graph unit test

--- tests/Unit/GraphTest.php
+++ tests/Unit/GraphTest.php
@@ -3,142 +3,205 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use Kit\Container\Graph;
-use Kit\Contracts\Interfaces\Graph as IGraph;
-use Kit\Entity\Graph as Node;
 
+use Kit\Structure\Graph\{Graph, Node};
+use Kit\Structure\Interfaces\{Graph as IGraph, GraphNode as IGraphNode};
 
-final class GraphTest extends TestCase {
+final class GraphTest extends TestCase
+{
     /**
      * @test
      */
-    public function createInstance(): IGraph {
-        $instance = new Graph;
-        
-        $this->assertIsObject($instance);
-        $this->assertInstanceOf(IGraph::class, $instance);
+    public function createInstance(): void
+    {
+        $instance = new Graph();
 
-        return $instance;
+        $this->assertIsObject($instance);
+        $this->assertInstanceOf(Graph::class, $instance);
+        $this->assertInstanceOf(IGraph::class, $instance);
     }
 
     /**
      * @test
-     * @depends createInstance
      */
-    public function isImplementedQueueInterface(IGraph $graph): void {
-        $interfaces = class_implements($graph::class);
+    public function isImplementedGraphInterface(): void
+    {
+        $interfaces = class_implements(Graph::class);
 
+        $this->assertIsArray($interfaces);
         $this->assertArrayHasKey(IGraph::class, $interfaces);
     }
 
     /**
      * @test
-     * @depends createInstance
      */
-    public function addNode(IGraph $graph): IGraph {
-        $values = ["User#1", "User#2"];
-        
-        $node1 = $graph->addNode(current($values));
-        $node2 = $graph->addNode(end($values));
+    public function newGraphShouldBeEmpty(): void
+    {
+        $graph = new Graph();
+        $state = $graph->toArray();
 
-        $store = $graph->toArray();
+        $this->assertSame([], $state["nodes"]);
+        $this->assertSame([], $state["edges"]);
+    }
 
-        # First Node
-        $this->assertIsObject($node1);
+    /**
+     * @test
+     */
+    public function addNode(): void
+    {
+        $graph = new Graph();
+
+        $node1 = $graph->addNode("User#1");
+        $node2 = $graph->addNode("User#2");
+
         $this->assertInstanceOf(Node::class, $node1);
-        $this->assertIsString($node1->value);
-        $this->assertNotNull($node1->value);
-
-        # Second Node
-        $this->assertIsObject($node2);
         $this->assertInstanceOf(Node::class, $node2);
-        $this->assertIsString($node2->value);
-        $this->assertNotNull($node2->value);
+        $this->assertSame("User#1", $node1->value);
+        $this->assertSame("User#2", $node2->value);
 
-        # Graph
-        $this->assertArrayHasKey(key:"nodes", array:$store);
-        $this->assertIsArray($store["nodes"]);
-        $this->assertIsIterable($store["nodes"]);
-        $this->assertCount(expectedCount:2, haystack:$store["nodes"]);
-        $this->assertSame($node1, current($store["nodes"]));
-        $this->assertSame($node2, end($store["nodes"]));
-        $this->assertInstanceOf($node1::class, current($store["nodes"]));
-        $this->assertInstanceOf($node2::class, end($store["nodes"]));
-
-
-        return $graph;
+        $state = $graph->toArray();
+        $this->assertCount(2, $state["nodes"]);
     }
 
     /**
      * @test
-     * @depends addNode
      */
-    public function getNodeIfExists(IGraph $graph): void {
-        $existsValues = ["User#1", "User#2"];
+    public function addNodeTrimsValue(): void
+    {
+        $graph = new Graph();
 
-        $node1 = $graph->getNode(current($existsValues));
-        $node2 = $graph->getNode(end($existsValues));
+        $node = $graph->addNode("  User#3  ");
 
-        # First Node
-        $this->assertNotNull($node1);
-        $this->assertIsObject($node1);
+        $this->assertSame("User#3", $node->value);
+    }
+
+    /**
+     * @test
+     */
+    public function getNodeIfExists(): void
+    {
+        $graph = new Graph();
+
+        $graph->addNode("User#1");
+        $graph->addNode("User#2");
+
+        $node1 = $graph->getNode("User#1");
+        $node2 = $graph->getNode("User#2");
+
         $this->assertInstanceOf(Node::class, $node1);
-        $this->assertIsString($node1->value);
-        
-        # Second Node
-        $this->assertNotNull($node2);
-        $this->assertIsObject($node2);
         $this->assertInstanceOf(Node::class, $node2);
-        $this->assertIsString($node2->value);
+        $this->assertSame("User#1", $node1->value);
+        $this->assertSame("User#2", $node2->value);
     }
 
     /**
      * @test
-     * @depends addNode
      */
-    public function getNodeIfNotExists(IGraph $graph): void {
-        $notExistsValues = ["User#3", "User#4"];
+    public function getNodeIfNotExists(): void
+    {
+        $graph = new Graph();
 
-        $node1 = $graph->getNode(current($notExistsValues));
-        $node2 = $graph->getNode(end($notExistsValues));
+        $graph->addNode("User#1");
 
-        $this->assertNull($node1);
-        $this->assertNull($node2);
+        $this->assertNull($graph->getNode("User#99"));
+        $this->assertNull($graph->getNode("Unknown"));
     }
 
     /**
      * @test
-     * @depends addNode
      */
-    public function addEdge(IGraph $graph): void {
-        $existsValues = ["User#1", "User#2"];
+    public function addEdgeCreatesBidirectionalConnection(): void
+    {
+        $graph = new Graph();
 
-        $node1 = $graph->getNode(current($existsValues));
-        $node2 = $graph->getNode(end($existsValues));
+        $node1 = $graph->addNode("A");
+        $node2 = $graph->addNode("B");
 
         $graph->addEdge($node1, $node2);
 
-        $store = $graph->toArray();
+        $edges = $graph->toArray()["edges"];
 
-        $this->assertArrayHasKey(key:"edges", array:$store);
-        $this->assertIsArray($store["edges"]);
-        $this->assertIsIterable($store["edges"]);
-        $this->assertCount(expectedCount:2, haystack:$store["edges"]);
-        $this->assertInstanceOf(Node::class, current($store["edges"][$node1->value]));
-        $this->assertInstanceOf(Node::class, end($store["edges"][$node1->value]));
-        $this->assertEquals($node2->value, current($store["edges"][$node1->value])->value);
-        $this->assertEquals($node1->value, end($store["edges"][$node2->value])->value);
+        $this->assertArrayHasKey("A", $edges);
+        $this->assertArrayHasKey("B", $edges);
+        $this->assertCount(1, $edges["A"]);
+        $this->assertCount(1, $edges["B"]);
+        $this->assertSame($node2, $edges["A"][0]);
+        $this->assertSame($node1, $edges["B"][0]);
     }
 
     /**
      * @test
-     * @depends addNode
      */
-    public function getStateToAnArray(IGraph $graph): void {
-        $store = $graph->toArray();
+    public function canAddMultipleEdges(): void
+    {
+        $graph = new Graph();
 
-        $this->assertIsArray($store);
-        $this->assertIsIterable($store);
-        $this->assertCount(expectedCount:2, haystack:$store);
+        $a = $graph->addNode("A");
+        $b = $graph->addNode("B");
+        $c = $graph->addNode("C");
+
+        $graph->addEdge($a, $b);
+        $graph->addEdge($a, $c);
+
+        $edges = $graph->toArray()["edges"];
+
+        $this->assertCount(2, $edges["A"]);
+        $this->assertCount(1, $edges["B"]);
+        $this->assertCount(1, $edges["C"]);
+    }
+
+    /**
+     * @test
+     */
+    public function toArrayReturnsCorrectStructure(): void
+    {
+        $graph = new Graph();
+
+        $node = $graph->addNode("Root");
+        $graph->addEdge($node, $graph->addNode("Child"));
+
+        $state = $graph->toArray();
+
+        $this->assertArrayHasKey("nodes", $state);
+        $this->assertArrayHasKey("edges", $state);
+        $this->assertCount(2, $state["nodes"]);
+        $this->assertNotEmpty($state["edges"]);
+    }
+
+    /**
+     * @test
+     */
+    public function nodeImplementsGraphNodeInterface(): void
+    {
+        $node = new Node("test");
+
+        $this->assertInstanceOf(IGraphNode::class, $node);
+    }
+
+    /**
+     * @test
+     */
+    public function nodeCanStoreMetaData(): void
+    {
+        $node = new Node("User#1");
+
+        $node->addMeta(["role" => "admin"]);
+        $node->addMeta("active");
+
+        $meta = $node->getMeta();
+
+        $this->assertCount(2, $meta);
+        $this->assertSame(["role" => "admin"], $meta[0]);
+        $this->assertSame("active", $meta[1]);
+    }
+
+    /**
+     * @test
+     */
+    public function nodeMetaStartsEmpty(): void
+    {
+        $node = new Node("User#1");
+
+        $this->assertSame([], $node->getMeta());
     }
 }
